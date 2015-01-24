e0f82042d86ed0de2fe11d45af2f680202a80095
ryan <ryanmwheeler@gmail.com>
2015-01-24 14:15:35 -0600

    add method

--- src/main/scala/com/brokendata/scrapers/GoogleScraper.scala
+++ src/main/scala/com/brokendata/scrapers/GoogleScraper.scala
@@ -1,15 +1,19 @@
 package com.brokendata.scrapers
 import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
 
 object GoogleShoppingScraper extends Scraper {
 
-  def downloadURL(page: String):
-
-  def downloadPrices(page: String): String = {
-    val Doc = Jsoup.connect(page)
+  def downloadURL(url: String): Document = {
+    val Doc = Jsoup.connect(url)
       .userAgent("Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.114 Safari/537.36")
       .get()
 
+    Doc
+  }
+
+  def downloadPrices(url: String): String = {
+    val Doc = downloadURL(url)
     val Element = Doc.select("#rso li._Ked ol li div._tyb")
 
     Element.text()
