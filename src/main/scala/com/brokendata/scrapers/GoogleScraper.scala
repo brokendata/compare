0017312d545c66386bc9bedac201ab3ec919730c
ryan <ryanmwheeler@gmail.com>
2015-01-24 14:19:12 -0600

    add2trait

--- src/main/scala/com/brokendata/scrapers/GoogleScraper.scala
+++ src/main/scala/com/brokendata/scrapers/GoogleScraper.scala
@@ -4,7 +4,7 @@ import org.jsoup.nodes.Document
 
 object GoogleShoppingScraper extends Scraper {
 
-  def downloadURL(url: String): Document = {
+  def downloadHTML(url: String): Document = {
     val Doc = Jsoup.connect(url)
       .userAgent("Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.114 Safari/537.36")
       .get()
@@ -13,7 +13,7 @@ object GoogleShoppingScraper extends Scraper {
   }
 
   def downloadPrices(url: String): String = {
-    val Doc = downloadURL(url)
+    val Doc = downloadHTML(url)
     val Element = Doc.select("#rso li._Ked ol li div._tyb")
 
     Element.text()
--- src/main/scala/com/brokendata/scrapers/Scraper.scala
+++ src/main/scala/com/brokendata/scrapers/Scraper.scala
@@ -1,7 +1,8 @@
 package com.brokendata.scrapers
-
+import org.jsoup.nodes.Document
 
 trait Scraper {
+  def downloadHTML(url: String): Document
   def downloadPrices(page: String): String
 }
 
